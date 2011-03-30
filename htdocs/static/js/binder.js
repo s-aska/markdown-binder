869120603dcbe558a95d74cd9f2a110ff469142d
s-aska <s.aska.org@gmail.com>
2011-03-30 09:12:21 +0900

    fix naviko.

--- htdocs/static/js/binder.js
+++ htdocs/static/js/binder.js
@@ -1,8 +1,7 @@
-function MarkdownBinder() {
-}
+(function(ns, w, d) {
 
-MarkdownBinder.prototype = {
-
+ns.MarkdownBinder = initialize;
+ns.MarkdownBinder.prototype = {
     expanded: false,
     gone: false,
     path: false,
@@ -10,203 +9,214 @@ MarkdownBinder.prototype = {
     dir: false,
     dirs: [],
     title: '',
+    catfile: catfile,
+    go: go,
+    load: load,
+    expand: expand,
+    initApplication: initApplication,
+    initHeight: initHeight,
+    initPagelink: initPagelink,
+    initDocument: initDocument,
+    initSidebar: initSidebar,
+    initHighlight: initHighlight
+};
 
-    catfile: function(dir, file){
-        if (dir == '/') {
-            return dir + file;
-        } else {
-            return dir + '/' + file;
-        }
-    },
+function initialize(options){
 
-    // navigation
-    go: function(path, callback){
-        if (history.pushState) {
-            history.pushState(path, '', location.protocol + '//' + location.host + path);
-        }
-        this.load(path, callback);
-    },
+}
 
-    load: function(path, callback){
-        var binder = this;
-        var url = path;
-        if (url == '/') {
-            url = url + 'TOP';
-        }
-        if (jQuery.browser.msie) {
-            url = encodeURI(url);
-        }
-        var match = path.match(/^(.*)\/([^\/]*)$/);
-        $.ajax({
-            url: url + '.html',
-            cache: false,
-            success: function(html){
-                binder.gone = true;
-                binder.initDocument(html);
-                binder.path = path;
-                binder.dir = match[1];
-                binder.basename = match[2];
-                binder.initPagelink();
-                binder.initHighlight();
-                if (path == '/') {
-                    $('title').text(binder.title);
-                } else {
-                    $('title').text(binder.title + url.replace('/', ' - '));
-                }
-                if (callback) {
-                    callback();
-                }
-            },
-            error: function(XMLHttpRequest, status, errorThrown){
-                $('article section').html(status + ': ' + errorThrown);
-                $('article nav').hide();
-            }
-        });
-    },
-
-    expand: function(){
-        var binder = this;
-        if (binder.expanded) {
-            $('aside nav ul ul').hide();
-            $('aside nav li.dir').addClass('close');
-            $('#expand').addClass('close');
-            binder.expanded = false;
-            binder.initHighlight();
-        } else {
-            $('aside nav ul ul').show();
-            $('aside nav li.dir').removeClass('close');
-            $('#expand').removeClass('close');
-            binder.expanded = true;
-            binder.initHeight();
-        }
-    },
-
-    // initXXX
-    initApplication: function(){
-        var binder = this;
-
-        // history control
-        if (history.pushState) {
-            // history.pushState('/', '', location.protocol + '//' + location.host + location.pathname);
-            // browser go back event
-            window.addEventListener('popstate', function (event) {
-                if (binder.gone) {
-                    binder.load(event.state);
-                }
-            });
-        }
-
-        // bind
-        $('#expand').bind('click', function(){binder.expand();return false;});
-        $('#expand').hover(function(){
-            $(this).css('cursor','pointer');
-            $(this).addClass('highlight');
-        },function(){
-            $(this).css('cursor','default');
-            $(this).removeClass('highlight');
-        });
-        $('#expand').show();
-
-        // init 
-        binder.path = location.pathname;
-        binder.initSidebar();
-        binder.initPagelink();
-        binder.initDocument();
-        binder.initHeight();
-        binder.title = $('header h1').text();
-    },
-
-    initHeight: function(){
-        $('article').removeAttr('style');
-        $('aside').removeAttr('style');
-        var sidebar_height = $('aside').height();
-        var document_height = $('article').height();
-        if (sidebar_height > document_height) {
-            $('article').height(sidebar_height);
-        } else {
-            $('aside').height(document_height);
-        }
-    },
-
-    initPagelink: function() {
-        var binder = this;
-        $('article nav').show();
-        $('#permalink').attr('href', binder.path);
-    },
-
-    initDocument: function(html){
-        var binder = this;
-        if (html) {
-            $('article section').html(html);
-            binder.initHeight();
-        }
-        $('article section').find('a').each(function(){
-            if ($(this).attr('href').match(/^https?:/)) {
-                $(this).attr('target', '_blank');
-            } else {
-                $(this).bind('click', function(){ binder.go($(this).attr('href')); return false;});
-            }
-        });
-    },
-
-    initSidebar: function(){
-        var binder = this;
-        $('aside li.file a').each(function(){
-            var a = $(this);
-            a.bind('click', function(){
-                binder.go(a.attr('href'));
-                return false;
-            });
-        });
-        $('aside li.dir').hover(function(){
-            $(this).css('cursor','pointer');
-            $(this).addClass('highlight');
-        },function(){
-            $(this).css('cursor','default');
-            $(this).removeClass('highlight');
-        });
-        
-        $('aside li.dir').click(function(){
-            var dir = $(this);
-            var ul = dir.children();
-            if (ul.length > 0 && ul.get(0).tagName.toLowerCase() == 'ul') {
-                var display = ul.css('display');
-                if (display == "" || display == "none") {
-                    ul.slideDown('fast', function(){dir.removeClass('close');binder.initHeight();});
-                } else {
-                    ul.slideUp('fast', function(){dir.addClass('close')});
-                }
-            }
-            return false;
-        });
-        $('aside ul ul').hide();
-        $('aside ul li.dir').addClass('close');
-        binder.initHighlight();
-    },
-    
-    initHighlight: function(){
-        var binder = this;
-        $('aside nav li a').each(function(){
-            var li = $(this).parent();
-            var a = $(this);
-            var file = a.attr('href');
-            if (file && (binder.path == file || binder.path == encodeURI(file))) {
-                a.addClass('highlight');
-                li.parents('ul').show();
-            } else {
-                a.removeClass('highlight');
-            }
-        });
-        $('aside nav li.dir').each(function(){
-            var dir = $(this);
-            var file = dir.data('file');
-            var fileEncode = encodeURI(file);
-            var re = new RegExp('^' + file + '/');
-            var reEncode = new RegExp('^' + fileEncode + '/');
-            if (file && (binder.path.match(re) || binder.path.match(reEncode))) {
-                dir.removeClass('close');
-            }
-        });
-        binder.initHeight();
+function catfile(dir, file){
+    if (dir == '/') {
+        return dir + file;
+    } else {
+        return dir + '/' + file;
     }
 }
+
+function go(path, callback){
+    if (history.pushState) {
+        history.pushState(path, '', location.protocol + '//' + location.host + path);
+    }
+    this.load(path, callback);
+}
+
+function load(path, callback){
+    var binder = this;
+    var url = path;
+    if (url == '/') {
+        url = url + 'TOP';
+    }
+    if ($.browser.msie) {
+        url = encodeURI(url);
+    }
+    var match = path.match(/^(.*)\/([^\/]*)$/);
+    $.ajax({
+        url: url + '.html',
+        cache: false,
+        success: function(html){
+            binder.gone = true;
+            binder.initDocument(html);
+            binder.path = path;
+            binder.dir = match[1];
+            binder.basename = match[2];
+            binder.initPagelink();
+            binder.initHighlight();
+            if (path == '/') {
+                $('title').text(binder.title);
+            } else {
+                $('title').text(binder.title + url.replace('/', ' - '));
+            }
+            if (callback) {
+                callback();
+            }
+        },
+        error: function(XMLHttpRequest, status, errorThrown){
+            $('article section').html(status + ': ' + errorThrown);
+            $('article nav').hide();
+        }
+    });
+}
+
+function expand(){
+    if (this.expanded) {
+        $('aside nav ul ul').hide();
+        $('aside nav li.dir').addClass('close');
+        $('#expand').addClass('close');
+        this.expanded = false;
+        this.initHighlight();
+    } else {
+        $('aside nav ul ul').show();
+        $('aside nav li.dir').removeClass('close');
+        $('#expand').removeClass('close');
+        this.expanded = true;
+        this.initHeight();
+    }
+}
+
+function initApplication(){
+    var binder = this;
+
+    // history control
+    if (history.pushState) {
+        // history.pushState('/', '', location.protocol + '//' + location.host + location.pathname);
+        // browser go back event
+        window.addEventListener('popstate', function (event) {
+            if (binder.gone) {
+                binder.load(event.state);
+            }
+        });
+    }
+
+    // bind
+    $('#expand').bind('click', function(){binder.expand();return false;});
+    $('#expand').hover(function(){
+        $(this).css('cursor','pointer');
+        $(this).addClass('highlight');
+    },function(){
+        $(this).css('cursor','default');
+        $(this).removeClass('highlight');
+    });
+    $('#expand').show();
+
+    // init 
+    this.path = location.pathname;
+    this.initSidebar();
+    this.initPagelink();
+    this.initDocument();
+    this.initHeight();
+    this.title = $('header h1').text();
+}
+
+function initHeight(){
+    $('article').removeAttr('style');
+    $('aside').removeAttr('style');
+    var sidebar_height = $('aside').height();
+    var document_height = $('article').height();
+    if (sidebar_height > document_height) {
+        $('article').height(sidebar_height);
+    } else {
+        $('aside').height(document_height);
+    }
+}
+
+function initPagelink(){
+    $('article nav').show();
+    $('#permalink').attr('href', this.path);
+}
+
+function initDocument(html){
+    var binder = this;
+    if (html) {
+        $('article section').html(html);
+        this.initHeight();
+    }
+    $('article section').find('a').each(function(){
+        if ($(this).attr('href').match(/^https?:/)) {
+            $(this).attr('target', '_blank');
+        } else {
+            $(this).bind('click', function(){ binder.go($(this).attr('href')); return false;});
+        }
+    });
+}
+
+function initSidebar(){
+    var binder = this;
+    $('aside li.file a').each(function(){
+        var a = $(this);
+        a.bind('click', function(){
+            binder.go(a.attr('href'));
+            return false;
+        });
+    });
+    $('aside li.dir').hover(function(){
+        $(this).css('cursor','pointer');
+        $(this).addClass('highlight');
+    },function(){
+        $(this).css('cursor','default');
+        $(this).removeClass('highlight');
+    });
+    $('aside li.dir').click(function(){
+        var dir = $(this);
+        var ul = dir.children();
+        if (ul.length > 0 && ul.get(0).tagName.toLowerCase() == 'ul') {
+            var display = ul.css('display');
+            if (display == "" || display == "none") {
+                ul.slideDown('fast', function(){dir.removeClass('close');binder.initHeight();});
+            } else {
+                ul.slideUp('fast', function(){dir.addClass('close')});
+            }
+        }
+        return false;
+    });
+    $('aside ul ul').hide();
+    $('aside ul li.dir').addClass('close');
+    this.initHighlight();
+}
+
+function initHighlight(){
+    var binder = this;
+    $('aside nav li a').each(function(){
+        var li = $(this).parent();
+        var a = $(this);
+        var file = a.attr('href');
+        if (file && (binder.path == file || binder.path == encodeURI(file))) {
+            a.addClass('highlight');
+            li.parents('ul').show();
+        } else {
+            a.removeClass('highlight');
+        }
+    });
+    $('aside nav li.dir').each(function(){
+        var dir = $(this);
+        var file = dir.data('file');
+        var fileEncode = encodeURI(file);
+        var re = new RegExp('^' + file + '/');
+        var reEncode = new RegExp('^' + fileEncode + '/');
+        if (file && (binder.path.match(re) || binder.path.match(reEncode))) {
+            dir.removeClass('close');
+        }
+    });
+    this.initHeight();
+}
+
+})(this, this, document);
